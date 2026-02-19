3ae1c0ca8fd8637100a69075a0cc3f8dc2e5d60e
ggagliola <ggagliol@iu.edu>
2026-02-19 10:17:54 -0500

    Downloaded Interactivity in Media

---
+++ Interactivity in Media/level 1/index.js
@@ -0,0 +1,2 @@
+// Get button and box elements -> Listen for a click -> Change the appearance by toggling CSS class
+
---
+++ Interactivity in Media/level 2/script.js
@@ -0,0 +1,20 @@
+// STEP 1: Get the button from the HTML using its id
+// This allows JavaScript to know which button we are talking about
+
+
+
+
+
+// STEP 2: Get the  bulbs (left and right) element from the HTML
+// We store it in a variables(one for each) so we can change it later
+
+
+
+
+
+// Tell JavaScript to listen for a click on the button
+// addEventListener waits until the user clicks the button and Toggle the "on" class/styling on bulbs
+
+
+
+
