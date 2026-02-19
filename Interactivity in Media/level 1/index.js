bc4946588f051f11e6ab88e744289e7b4c32f327
ggagliola <ggagliol@iu.edu>
2026-02-19 10:41:52 -0500

    JavaScript Lecture

--- Interactivity in Media/level 1/index.js
+++ Interactivity in Media/level 1/index.js
@@ -1,2 +1,8 @@
 // Get button and box elements -> Listen for a click -> Change the appearance by toggling CSS class
 
+let box = document.getElementById("box");
+let button = document.getElementById("btn");
+
+button.addEventListener("click", function(){
+    box.classList.toggle("blue")
+})
--- Interactivity in Media/level 2/script.js
+++ Interactivity in Media/level 2/script.js
@@ -1,6 +1,7 @@
 // STEP 1: Get the button from the HTML using its id
 // This allows JavaScript to know which button we are talking about
 
+let lightButton = document.getElementById("lightButton");
 
 
 
@@ -8,13 +9,17 @@
 // STEP 2: Get the  bulbs (left and right) element from the HTML
 // We store it in a variables(one for each) so we can change it later
 
-
+let leftBulb = document.getElementById("leftBulb");
+let rightBulb = document.getElementById("leftBulb");
 
 
 
 // Tell JavaScript to listen for a click on the button
 // addEventListener waits until the user clicks the button and Toggle the "on" class/styling on bulbs
 
-
+button.addEventListener("click", function(){
+    leftBulb.classList.toggle("on");
+    rightBulb.classList.toggle("on");
+})
 
 
